c694d8d8006bbde6204f47c0b377bd2535a36afe
Ashk3000 <git@ashk3000.com>
2024-07-20 13:57:20 -0400

    Add project files.

---
+++ plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx.Configuration;
+using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using GameNetcodeStuff;
+
+namespace SuitFix
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalLogoMod : BaseUnityPlugin
+    {
+        public const string modGUID = "com.ashk3000.SuitFix";
+        public const string modName = "SuitFix";
+        public const string modVersion = "1.0.0";
+
+        public static ConfigEntry<string> imagePath;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private void Awake()
+        {
+            harmony.PatchAll(typeof(MenuManagerPatch));
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    class MenuManagerPatch
+    {
+        [HarmonyPatch("SetHoverTipAndCurrentInteractTrigger")]
+        [HarmonyPostfix]
+        static void Postfix(PlayerControllerB __instance)
+        {
+
+            if (__instance.cursorTip.text == "Change suit")
+            {
+                __instance.cursorTip.text = "Change: Decoy suit";
+            }
+        }
+    }
+}
